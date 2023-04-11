7c50a89dcefd823e0968df0db8038ee4ac2f513c
jason anderson <jason.anderson.professional@gmail.com>
2023-04-11 14:56:12 -0500

    refactor to config yaml

--- dags/pytorch_pipeline/pytorch_docker.py
+++ dags/pytorch_pipeline/pytorch_docker.py
@@ -6,28 +6,27 @@ from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 import docker
 from docker.types import Mount
+import yaml
 
-
+config_file = "/opt/airflow/dags/pytorch_pipeline/config.yaml"
+with open(config_file, "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
 default_args = {
     'owner': 'airflow',
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
     'start_date': days_ago(1),
+    
 }
 
-
-
-
 dag = DAG(
-    'pytorch_docker_dag',
+    'pytorch_training_pipeline',
     default_args=default_args,
     description='PyTorch container with DockerOperator',
     schedule_interval=timedelta(minutes=5),
     catchup=False,
 )
-
-
 
 start = DummyOperator(
     task_id='start_pipeline',
@@ -39,50 +38,32 @@ stop = DummyOperator(
     dag=dag,
 )
 
-
-
-image = "dockeroperator_deploy_image:latest"
-dockerfile_dir = "/opt/airflow/dags/pytorch_pipeline"
-
 pytorch_build = BashOperator(
-    task_id='build_docker_image',
-    bash_command=f'docker build -t {image} {dockerfile_dir}',
+    task_id='build_docker_image_for_training',
+    bash_command=f"docker build -t {config['training_image']} {config['dockerfile_dir']}",
     dag=dag,
 )
 
-host_path = '/c/Users/kraut/Documents/My_Code/development_template_with_airflow/scripts'
-#host_path = '/d/scripts'
-container_path = '/tmp/scripts'
-
 pytorch_task = DockerOperator(
-    task_id='run_pytorch_container',
+    task_id='run_pytorch_training_from_docker',
     api_version='auto',
     container_name = 'trainer',
-
-    image=image,
-    #image="nvidia/cuda:11.4.0-cudnn8-runtime-ubuntu20.04",
-    #image='my_pytorch',
-
-    command = f'/bin/bash -c "date;id;date;date;cd ~; echo xxxxxxxxxxxxxxxxxxxxxx;nvidia-smi;ls -latrs {container_path}; python {container_path}/run_this.py" ',
-    
+    image=config['training_image'],
+    command = f"python {config['container_path']}/{config['training_executable']}",
     mounts=[
-        Mount(source=host_path, 
-              target=container_path, 
+        Mount(source=config['host_path'], 
+              target=config['container_path'], 
               type="bind",
               ),
-    ],
+            ],
     auto_remove=True,
     user='root',
     privileged = True,
     docker_url='unix://var/run/docker.sock',
     mount_tmp_dir=False, 
-    
     device_requests=[
-    docker.types.DeviceRequest(count=-1, capabilities=[['gpu']]),
-    ],
+    docker.types.DeviceRequest(count=-1, capabilities=[['gpu']]),],
     network_mode='bridge',
-
     dag=dag,
 )
-#build_docker_image >> pytorch_task
 start >> pytorch_build >> pytorch_task >> stop
